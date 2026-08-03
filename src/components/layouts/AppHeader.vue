9f569c7cb856bfcd17f883c6f9145fba0a645932
JuanMaurelia <33904555+PlanumCL@users.noreply.github.com>
2026-08-03 00:10:43 -0400

    feat: implement Avatar component suite and integrate into AppHeader UI

--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { Button } from '@/components/ui/button'
@@ -11,10 +12,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { ChevronDown } from '@lucide/vue'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+// import { ChevronDown } from '@lucide/vue'
 
 const router = useRouter()
 const authStore = useAuthStore()
+
+const userInitial = computed(
+  () => authStore.profile?.full_name?.trim().charAt(0).toUpperCase() ?? '',
+)
 
 async function onLogout() {
   await authStore.logout()
@@ -28,15 +34,25 @@ async function onLogout() {
   >
     <SidebarTrigger />
 
+    <!-- user_data -->
     <DropdownMenu>
       <DropdownMenuTrigger as-child>
-        <Button variant="outline" class="cursor-pointer ml-auto">
-          {{ authStore.profile?.full_name || 'Perfil' }}
-          <ChevronDown class="ml-2 h-4 w-4" />
-        </Button>
+        <Avatar class="cursor-pointer">
+          <!-- <AvatarImage :src="authStore.profile?.avatar_url || 'https://github.com/shadcn.png'" /> -->
+          <AvatarFallback>{{ userInitial }}</AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
+      <DropdownMenuContent align="end" class="min-w-60">
+        <DropdownMenuLabel class="flex items-center gap-2">
+          <Avatar class="cursor-pointer">
+            <!-- <AvatarImage :src="authStore.profile?.avatar_url || 'https://github.com/shadcn.png'" /> -->
+            <AvatarFallback>{{ userInitial }}</AvatarFallback>
+          </Avatar>
+          <div>
+            <h6 class="font-semibold text-black text-sm">{{ authStore.profile?.full_name }}</h6>
+            <p class="text-muted-foreground">{{ authStore.user?.email }}</p>
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem class="h-8 px-3 cursor-pointer">Perfil</DropdownMenuItem>
         <DropdownMenuSeparator />
